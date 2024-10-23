a84e6d9b4ec43ea2b7226fd6228d470064fbf423
“WKayyy” <w2570900770@outlook.com>
2024-10-23 08:57:50 +0800

    10.23

---
+++ Project3/Project3/code.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 5;
+int main() {
+	char stuid[10], name[20];
+	float score;
+	FILE* outfile;
+	int i;
+	outfile = fopen_s("scores.txt", "w");
+	if (outfile == NULL) {
+		printf("不能创建文件！");
+		exit(0);
+	}
+	fprintf(outfile, "学号\t姓名\t入学成绩\n");
+		for (i = 0, i < N; i++) {
+			printf("请输入学生学号、姓名和入学成绩：");
+			scanf_s("%sd%s%f", stuid, name, &score);
+			fprintf(outfile, "%s\t%s\t%f\n", stuid, name, score);
+		}
+		fclose(outfile);
+		return 0;
+}
