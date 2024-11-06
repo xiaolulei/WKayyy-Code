024cc1d80e1e1d54296d44faa8593d3ef98bc2e4
“WKayyy” <w2570900770@outlook.com>
2024-11-07 00:16:36 +0800

    code

--- TOOLS/luogu/luoguP1125.c
+++ TOOLS/luogu/luoguP1125.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 int main(){
     int maxn=0,minn=0;
+    char a[101];
+    gets(a);
+    int len = sizeof(a) / sizeof(a[0]);
+    int b[len];
+    int sum=0;
+    for(int i=0;a[i];i++){
+        for(int j=len;j>i;j++){
+            sum++;
+            if(a[i]==a[j]) b[sum]++;
+        }
+    }
     
-
-
     return 0;
 }
