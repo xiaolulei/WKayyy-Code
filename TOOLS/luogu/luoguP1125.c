38e1ca883e7713f649de4b605fc86726e7250ebd
“WKayyy” <w2570900770@outlook.com>
2024-11-07 23:29:38 +0800

    code

--- TOOLS/luogu/luoguP1125.c
+++ TOOLS/luogu/luoguP1125.c
@@ -1,17 +1,35 @@
 #include<stdio.h>
+#include<math.h>
+
+int a [26];
 int main(){
-    int maxn=0,minn=0;
-    char a[101];
-    gets(a);
-    int len = sizeof(a) / sizeof(a[0]);
-    int b[len];
-    int sum=0;
-    for(int i=0;a[i];i++){
-        for(int j=len;j>i;j++){
-            sum++;
-            if(a[i]==a[j]) b[sum]++;
+    char b[110];
+    gets(b);
+    for(int i = 0;b[i];i++){
+        a[ b[i] - 'a' ] ++;
+    }
+    int min = 101 , max = -1;
+    for(int i = 0;i < 25;i++){
+        if(a[i]>0){
+            if(a[i]>max) max = a[i];
+            if(a[i]<min) min = a[i];
         }
     }
-    
+    int t = max - min;
+    int flag=0;
+    if(t>2){
+        for(int i=2;i < sqrt(t);i++){
+            if(t%i==0){
+                flag = 1;
+                break;
+            }
+        }
+    }
+    if(flag||t==1||t==0){
+        printf("No Answer\n0");
+    }
+    else{
+        printf("Lucky Word\n%d",t);
+    }
     return 0;
 }
