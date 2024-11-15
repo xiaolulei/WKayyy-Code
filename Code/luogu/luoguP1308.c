a09f3f094e556f069ab360a124072f6e2df017f9
WKayyy <w2570900770@outlook.com>
2024-11-15 15:27:01 +0800

    Code

--- Code/luogu/luoguP1308.c
+++ Code/luogu/luoguP1308.c
@@ -8,24 +8,28 @@ int mian(){
     scanf("%s",b);
     getchar();
     int flag=-1;
-    int sum=0;
+    int sum=0,sum1=0;
     int len1=strlen(a);
     int len2=strlen(b);
     int i=1;
-        for(int j=0;j<=len2-2;){
-           if(a[0]==b[j]||a[0]==b[j]+32||a[0]==b[j]-32){
-            while(i<=len1-2){
-                if(a[i]==b[j]||a[i]==b[j]+32||a[i]==b[j]-32){
-                    i++;
-                    sum++;
+    int sum3[100];
+    for(int i=0;i<=len2-2;){
+        if(b[i]==a[0]){
+            for(int j=1;j<=len2-2;){
+                if(b[i]==a[j]) {
+                j++;
+                sum++;
                 }
-                else break;
-            }
-           }
-
-
+                else {
+                    sum=0;
+                    break;
+                }
+                if(sum==len1) sum1++;
         }
-    
-
+        
+        
+        
+        
+        printf("%d",sum1);
     return 0;
 }
