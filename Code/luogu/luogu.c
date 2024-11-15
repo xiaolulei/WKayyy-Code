ab7ea6e961a7092b32dbeb95d69d4a826064ad07
WKayyy <w2570900770@outlook.com>
2024-11-15 15:08:48 +0800

    Code

---
+++ Code/luogu/luogu.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<strlen.h>
+char b[1000000];
+int main(){
+    char a[20];
+    scanf("%s",a);
+    scanf("%s",b);
+    int len1=strlen(a);
+    for(int i=0;i<=len-1;){
+        
+
+
+    return 0;
+    }
