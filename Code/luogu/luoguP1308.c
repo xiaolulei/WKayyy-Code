d79c275d37f1fc13ae058d7ef53a30ea3972e6dd
“WKayyy” <w2570900770@outlook.com>
2024-11-16 17:33:49 +0800

    code

--- Code/luogu/luoguP1308.c
+++ Code/luogu/luoguP1308.c
@@ -1,35 +1,32 @@
 #include<stdio.h>
 #include<string.h>
 char b[1000000];
-int mian(){
+int main(){
     char a[20];
     scanf("%s",a);
     getchar();
     scanf("%s",b);
     getchar();
-    int flag=-1;
     int sum=0,sum1=0;
     int len1=strlen(a);
     int len2=strlen(b);
-    int i=1;
-    int sum3[100];
     for(int i=0;i<=len2-2;){
         if(b[i]==a[0]){
-            for(int j=1;j<=len2-2;){
-                if(b[i]==a[j]) {
-                j++;
-                sum++;
+            for(int j=1;j<=len1-2;){
+                if(b[i]==a[j]||b[i]==a[j]+32||b[i]==a[j]-32){
+                    j++;
+                    i++;
+                    sum++;
+                    if(sum==len1) break;
                 }
                 else {
                     sum=0;
                     break;
                 }
-                if(sum==len1) sum1++;
+            }
+            if(sum==len1) sum1++;
         }
-        
-        
-        
-        
-        printf("%d",sum1);
+    }
+    printf("%d",sum1);
     return 0;
 }
--- Code/touge/shiyan2/shiyan4/4.c
+++ Code/touge/shiyan2/shiyan4/4.c
@@ -1,17 +1,65 @@
 #include<stdio.h>
 #include<string.h>
+int bubbleup(int sum);
+int bubbledown(int sum);
+void blackhole(int num);
 int main(){
-    char a[10];
-    int sum[10];
-    scanf("%s",a);
-    int len=strlen(a);
-    for(int s=1;s<=7;s++){
-        for(int i=4;i>0;i--){
-            for(int j=1000;j>0;j/=10){
-            sum[]=sum1+a[i]*i*j;
+    int num;
+    scanf("%d",&num);
+    blackhole(num);
+    return 0;
+}
+void blackhole(int num){
+    int count=0;
+    while(num!=6174&&count<7){
+        int down = bubbledown(num);
+        int up = bubbleup(num);
+        num = up - down;
+        printf("%04d-%04d=%04d\n",up,down,num);
+        count++;
+    }
+}
+
+
+int bubbledown(int num){
+    int len=4;
+    int arr[4];
+    arr[0]=num%10;
+    arr[1]=(num/10)%10;
+    arr[2]=(num/100)%10;
+    arr[3]=(num/1000);
+    int i,j,temp=0;
+    for(i=0;i<len-1;i++){
+        for(j=0;j<len-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
     }
+    return arr[0]*1000+arr[1]*100+arr[2]*10+arr[3]*1;
+}
 
-    return 0;
+
+
+
+int bubbleup(int num){
+    int len=4;
+    int arr[4];
+    arr[0]=num%10;
+    arr[1]=(num/10)%10;
+    arr[2]=(num/100)%10;
+    arr[3]=(num/1000);
+    int i,j,temp=0;
+    for(i=0;i<len-1;i++){
+        for(j=0;j<len-1-i;j++){
+            if(arr[j]<arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr[0]*1000+arr[1]*100+arr[2]*10+arr[3]*1;
 }
