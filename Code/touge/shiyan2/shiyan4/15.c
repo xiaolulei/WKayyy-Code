ded811715589a165ffe3b25f08f77304f1176061
“WKayyy” <w2570900770@outlook.com>
2024-11-19 23:18:46 +0800

    code

--- Code/touge/shiyan2/shiyan4/15.c
+++ Code/touge/shiyan2/shiyan4/15.c
@@ -13,13 +13,11 @@ int main() {
     int a;
     scanf("%d", &a);
     int sum = ceil((float)a / 2);
-
     for (int i = 2; i < sum; i++) {
         int j = a - i;
         if (is_prime(i) && is_prime(j)) {
             printf("%d=%d+%d\n",a,i,j);
         }
     }
-
     return 0;
 }
