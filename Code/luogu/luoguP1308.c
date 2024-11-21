8460740d0d80852319ac9a54345914dddac87cea
“WKayyy” <w2570900770@outlook.com>
2024-11-21 18:01:17 +0800

    code

--- Code/luogu/luoguP1308.c
+++ Code/luogu/luoguP1308.c
@@ -1,27 +1,30 @@
 #include<stdio.h>
 #include<string.h>
-char b[1000001];
+char b[1000001];  
+int position[10000];
 int main()
 {
     int count=0;
     char a[20];
     gets(a);
     gets(b);
-    int position[100];
     int lena=strlen(a);
     int lenb=strlen(b);
     int p=0;
-    for(int i=0;i<lenb;i+=lena)
+    for(int i=0;i<lenb;i++)
     {
         int sum=0;
         for(int j=0;j<lena;j++){
             if(a[j]==b[i+j]||a[j]==b[i+j]+'a'-'A'||a[j]==b[i+j]+'A'-'a'){
-               sum++;
+                if((b[i-1]==' '&&b[i+lena]==' ')||(i==0&&b[i+lena]==' ')){
+                    sum++;
+                }
             }
         }
         if(sum==lena) {
             count++;
             position[p]=i;
+            p++;
         }
     }
     if(count==0) printf("-1");
