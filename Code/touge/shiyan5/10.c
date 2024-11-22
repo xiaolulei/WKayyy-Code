43b6fb4acce0426ff971704a5ef3e3f37bce385c
“WKayyy” <w2570900770@outlook.com>
2024-11-22 17:59:38 +0800

    code

--- Code/touge/shiyan5/10.c
+++ Code/touge/shiyan5/10.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
+
 int main(){
     int k1,k2;
+    int suma=0,sumb=0;
     scanf("%d%d",&k1,&k2);
     for(int i=k1;i<=k2;i++){
-        
+        int yu,sum1=0,sum2=0;
+        int i_=i;
+        yu=i_%2;
+        i_/=2;
+        if(yu==0) sum1++;
+        if(yu==1) sum2++;
+        if(sum2>sum1) suma++;
+        else sumb++;
     }
+    printf("%d %d\n",suma,sumb);
     return 0;
-}
+}//538 462
