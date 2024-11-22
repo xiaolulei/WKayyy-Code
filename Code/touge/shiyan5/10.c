7990aead2ab7d40c350d12255b6c4879355aa263
“WKayyy” <w2570900770@outlook.com>
2024-11-22 18:02:38 +0800

    code

--- Code/touge/shiyan5/10.c
+++ Code/touge/shiyan5/10.c
@@ -7,10 +7,14 @@ int main(){
     for(int i=k1;i<=k2;i++){
         int yu,sum1=0,sum2=0;
         int i_=i;
+        while(i_!=0){
         yu=i_%2;
         i_/=2;
         if(yu==0) sum1++;
         if(yu==1) sum2++;
+        
+        
+        }
         if(sum2>sum1) suma++;
         else sumb++;
     }
