831bef9c33413df1cf04e737f421aa25df131f80
“WKayyy” <w2570900770@outlook.com>
2024-12-05 22:42:13 +0800

    code

--- Code/zhizhen.c
+++ Code/zhizhen.c
@@ -1,17 +1,32 @@
 #include<stdio.h>
 int main()
 {
-
+    int score[4][7]={{1,80,82,95,88,93,438},
+                     {2,86,54,80,95,97,372},
+                     {3,80,70,56,88,93,387},
+                     {4,95,89,87,80,96,447}
+                    };
+    void sort(int(*p[])[7],int);
+    int n=4,(*p[4])[7];
+    sort(p,4);
+    for(int i=0;i<n;i++){
+        
+    }
     return 0;
 }
-int stringcmp(int *s1,int *s2){
-    while(*s1||*s2){
-        if(*s1-*s2){
-            break;
+void sort(int(*p[])[7],int n){
+    for(int i=0;i<n-1;i++){
+        int post=i;
+        for(int j=i+1;j<n;j++){
+            if(*(*p[i]+6)<*(*p[j]+6)){
+                post=j;
+            }
         }
-        else{
-            s1++;s2++;
+        if(post!=i){
+            int (*temp)[7];
+            temp=p[i];
+            p[i]=p[post];
+            p[post]=temp;
         }
     }
-    return *s1-*s2;
 }
