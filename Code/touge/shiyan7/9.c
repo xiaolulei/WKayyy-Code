adbc480607d2ac9ebc832c98fb0aa6d15d4e7fd3
WKayyy <w2570900770@outlook.com>
2024-12-11 16:24:51 +0800

    更新 9.c

--- Code/touge/shiyan7/9.c
+++ Code/touge/shiyan7/9.c
@@ -1,20 +1,7 @@
 #include<stdio.h>
-int num[31][31];
-int status[31][31];
 int main()
 {
-    int N;
-    int s=1;
-    scanf("%d",&N);
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            num[i][j]=s;
-            status[i][j]=1;
-            s++;
-        }
-    }
+    int n,i,status,j,k;
+    for(i
     return 0;
-}\
-
-
-// &
+}
