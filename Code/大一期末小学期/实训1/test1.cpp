4d969029f51c4e76393f33b2bbc0a7be625ee690
“WKayyy” <w2570900770@outlook.com>
2025-07-01 11:30:57 +0800

    更新作业代码，修改成文本形式打开文件

--- Code/大一期末小学期/实训1/test1.cpp
+++ Code/大一期末小学期/实训1/test1.cpp
@@ -25,7 +25,9 @@ struct Score
     float PhysicsScore;
     float ChemistryScore;
     float Score;
+    float aveScore = (ChineseScore + MathScore + EnglishScore + PhysicsScore + ChemistryScore) / 5;
 }StudentScore;
+
 void quick_sort(Score q[],int l, int r);
 void StudentSort();
 void InitiateFile();
@@ -52,7 +54,7 @@ void StudentSort()
     //我们假设学生数据不超过100
     Score temp[100];
     FILE *fp;
-    if((fp = fopen("StudentGrade.dat","rb"))==NULL)
+    if((fp = fopen("StudentGrade.dat","r"))==NULL)
     {
         system("pls");
         printf("\nCan not open StudentGraade correctly!\n");
@@ -62,7 +64,7 @@ void StudentSort()
     int pos = 0;
     while(fread(&temp[pos],sizeof(Score),1,fp)==1) pos++;
     fclose(fp);
-    fp = fopen("StudentGrade.dat","wb");
+    fp = fopen("StudentGrade.dat","w");
     if (fp == NULL) {
         cout << "Error opening file for writing!\n";
         exit(0);
@@ -102,11 +104,18 @@ void InitiateFile()
         if(ch_again == 'y' || ch_again == 'Y') break;
         else if (ch_again == 'n' || ch_again == 'N') return; 
     }
-    fp = fopen("StudentGrade.dat","wb");
+    fp = fopen("StudentGrade.dat","w");
     printf("\n\nInitialization success!\n");
     fclose(fp);
     system("pause");
     system("cls");
+}
+
+void ErrorGradePrint()
+{
+    printf("--------------------------------------------------------------");
+    printf("\n\n\t\tPlease Enter Correct Score!\n");
+    printf("\n--------------------------------------------------------------");
 }
 
 bool InputStudenInformation(FILE *fp)
@@ -159,49 +168,43 @@ bool InputStudenInformation(FILE *fp)
     CHI:printf("\nPlease Input Student's Chinese Grade:  "); scanf("%f",&t.ChineseScore); 
     if(t.ChineseScore>100||t.ChineseScore<0)
     {
-        printf("--------------------------------------------------------------");
-        printf("\n\n\t\tPlease Enter Correct Score!\n");
-        printf("\n--------------------------------------------------------------");
+        ErrorGradePrint();
         goto CHI;
     }
     MATH:printf("\nPlease Input Student's Math Grade:  "); scanf("%f",&t.MathScore); 
     if(t.MathScore>100||t.MathScore<0)
     {
-        printf("--------------------------------------------------------------");
-        printf("\n\n\t\tPlease Enter Correct Score!\n");
-        printf("\n--------------------------------------------------------------");
+        ErrorGradePrint();
         goto MATH;
     }
     ENG:printf("\nPlease Input Student's English Grade:  "); scanf("%f",&t.EnglishScore); 
     if(t.EnglishScore>100||t.EnglishScore<0)
     {
-        printf("--------------------------------------------------------------");
-        printf("\n\n\t\tPlease Enter Correct Score!\n");
-        printf("\n--------------------------------------------------------------");
+        ErrorGradePrint();
         goto ENG;
     }
     PHY:printf("\nPlease Input Student's Physics Grade:  "); scanf("%f",&t.PhysicsScore); 
     if(t.PhysicsScore>100||t.PhysicsScore<0)
     {
-        printf("--------------------------------------------------------------");
-        printf("\n\n\t\tPlease Enter Correct Score!\n");
-        printf("\n--------------------------------------------------------------");
+        ErrorGradePrint();
         goto PHY;
     }
     CHE:printf("\nPlease Input Student's Chemistry Grade:  "); scanf("%f",&t.ChemistryScore); 
     if(t.ChemistryScore>100||t.ChemistryScore<0)
     {
-        printf("--------------------------------------------------------------");
-        printf("\n\n\t\tPlease Enter Correct Score!\n");
-        printf("\n--------------------------------------------------------------");
+        ErrorGradePrint();
         goto CHE;
     }
     t.Score = t.ChemistryScore + t.ChineseScore + t.EnglishScore + t.MathScore + t.PhysicsScore ;
     fseek(fp, 0, SEEK_END);
-    if (fwrite(&t, sizeof(Score), 1, fp) != 1) {
-        printf("\nError writing to file!\n");
-        return false;
-    }
+    fprintf(fp, "%d;%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;\n",
+        t.StudentNum,
+        t.ChineseScore,
+        t.MathScore,
+        t.EnglishScore,
+        t.PhysicsScore,
+        t.ChemistryScore,
+        t.Score);
     return true;
 }
 
@@ -209,7 +212,7 @@ int main()
 {
     InitiateFile();
     FILE *fp;
-    if((fp = fopen("StudentGrade.dat","a+b")) == NULL)
+    if((fp = fopen("StudentGrade.dat","a+")) == NULL)
     {
         printf("--------------------------------------------------------------");
         printf("\n\n\tThe file is not initialized. Please initialize it first.\n");
@@ -230,7 +233,7 @@ int main()
     {
         StudentSort();
         printf("\n-----------------------------------------------------------------\n");
-        printf("")
+        printf("");
     }
     else if(command == 'N' || command == 'n' )
     {
