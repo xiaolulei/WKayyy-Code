760c7653a9beb67c4c91309654e533658469b275
“WKayyy” <w2570900770@outlook.com>
2025-07-02 11:07:22 +0800

    更新每日作业

--- Code/大一期末小学期/实训1/test1.cpp
+++ Code/大一期末小学期/实训1/test1.cpp
@@ -2,21 +2,10 @@
 #include <cstring>
 #include <cstdlib>
 #include <algorithm>
-
+#include <cstdio>
 using namespace std;
 
-/*
-实训题1
-
-使用结构体数组存放学生成绩信息，至少包含学号(整形)，语文、数学、英语、物理、化学这五门课程的百分制成绩。编写程序实现：
-(1) 编写一个函数用来输入所有学生的信息(当输入的学号为0时，结束输入)；
-(2)并将每个学生的全部数据保存到磁盘上的二进制文件StudentGrade.dat中。
-提示：二进制文件创建语句pf=fopen("student.dat","wb");写二进制文件语句fwrite(&stu[i],sizeof(Student),1,fp);
-*/
-
-int flag = 0;
-
-struct Score
+struct Score 
 {
     int StudentNum;
     float ChineseScore;
@@ -24,223 +13,279 @@ struct Score
     float EnglishScore;
     float PhysicsScore;
     float ChemistryScore;
-    float Score;
-    float aveScore = (ChineseScore + MathScore + EnglishScore + PhysicsScore + ChemistryScore) / 5;
-}StudentScore;
+    float TotalScore;
+    float AveScore;
+};
 
-void quick_sort(Score q[],int l, int r);
-void StudentSort();
-void InitiateFile();
-bool InputStudenInformation(FILE *fp);
-
-
-
-void quick_sort(Score q[],int l,int r){
-    if(l>=r) return;
-    int i=l-1,j=r+1;
-    float x=q[(l + r) >> 1].Score;
-    while(i<j){
-        do i++;while(q[i].Score<x);
-        do j--;while(q[j].Score>x);
-        if(i<j) swap(q[i],q[j]);
-    }
-    quick_sort(q,l,j);
-    quick_sort(q,j+1,r);
-}
-
-
-void StudentSort()
+void clearInputBuffer() 
 {
-    //我们假设学生数据不超过100
-    Score temp[100];
-    FILE *fp;
-    if((fp = fopen("StudentGrade.dat","r"))==NULL)
-    {
-        system("pls");
-        printf("\nCan not open StudentGraade correctly!\n");
-        system("pause");
-        exit(0);
-    }
-    int pos = 0;
-    while(fread(&temp[pos],sizeof(Score),1,fp)==1) pos++;
-    fclose(fp);
-    fp = fopen("StudentGrade.dat","w");
-    if (fp == NULL) {
-        cout << "Error opening file for writing!\n";
-        exit(0);
-    }
-
-    quick_sort(temp,0,pos-1);
-
-    for(int i = 0;i < pos; i++)
-    {
-        
-        if(fwrite(&temp[i],sizeof(Score),1,fp)!=1)
-        {
-            printf("\nError writing to file! System Stop !\n");
-            exit(0);
-        }
-    }
-    fclose(fp);
+    int c;
+    while((c=getchar())!='\n'&&c!=EOF);
 }
 
-void InitiateFile()
+bool initFile() 
 {
     char ch;
-    FILE *fp;
-    system("cls");//Do you want to initialize the file?
-    printf("\n\n\tDo you want to initialize the file?");
-    while (ch!='y' && ch!='Y' && ch!='n' && ch!='N'){
-        printf("\n\n\t\tContinue? (y/n)  ");
-        cin >> ch;
-        if(ch == 'y' || ch == 'Y') break;
-        else if (ch == 'n' || ch == 'N') return; 
+    system("cls");
+    printf("\n\tInitialize student grade file? (Initialization will clear all data)\n");
+    printf("\tPlease choose (y/n):  ");
+    cin>>ch;
+    if(ch!='y'&&ch!='Y') return true;
+
+    printf("\n\tConfirm to clear all data? (y/n):  ");
+    cin>>ch;
+    if(ch!='y'&&ch!='Y') return true;
+
+    FILE* fp=fopen("StudentGrade.dat","wb");
+    if(!fp) 
+    {
+        printf("\n\tFile initialization failed!\n");
+        return false;
     }
-    char ch_again;
-    printf("\n--------------------------------------------------------------------------------");
-    printf("\n\nThe initialization of file will lose all data in data files! Are you sure?(y/n)");
-    while (ch_again!='y' && ch_again!='Y' && ch_again!='n' && ch_again!='N'){
-        cin >> ch_again;
-        if(ch_again == 'y' || ch_again == 'Y') break;
-        else if (ch_again == 'n' || ch_again == 'N') return; 
-    }
-    fp = fopen("StudentGrade.dat","w");
-    printf("\n\nInitialization success!\n");
     fclose(fp);
+    printf("\n\tFile initialization successful!\n");
     system("pause");
     system("cls");
-}
-
-void ErrorGradePrint()
-{
-    printf("--------------------------------------------------------------");
-    printf("\n\n\t\tPlease Enter Correct Score!\n");
-    printf("\n--------------------------------------------------------------");
-}
-
-bool InputStudenInformation(FILE *fp)
-{
-    Input_Start:
-    Score x,t;
-    system("cls");
-    printf("\n\t\tStudents Information System\n");
-    printf("\n================================================================\n");
-    printf("\nPlease Input The Student's Information:  ");
-    printf("\n-----------------------------------------");
-    printf("\n\nPlease Input Student's num:  "); scanf("%d",&t.StudentNum); 
-    if(t.StudentNum==0)
-    {
-            printf("--------------------------------------------------------------");
-            printf("\n\n\t\tUser stops input!\n");
-            printf("\n--------------------------------------------------------------");
-            return false;
-    }
-    rewind(fp);
-    bool exists = false;
-    while (fread(&x, sizeof(Score), 1, fp) == 1) {
-        if (x.StudentNum == t.StudentNum) {
-            exists = true;
-            break;
-        }
-    }
-    
-    if (exists) {
-        char temp;
-        system("cls");
-        printf("================================================================");
-        printf("\n\nThe student %d has existed !  Input next? (y/n) :", t.StudentNum);
-        while (temp!='y' && temp!='Y' && temp!='n' && temp!='N') {
-            cin >> temp;
-            if (temp == 'y' || temp == 'Y') 
-                return true;
-            else if (temp == 'n' || temp == 'N')
-            {
-                system("cls");
-                printf("--------------------------------------------------------------");
-                printf("\n\n\t\tUser stops input!\n");
-                printf("\n--------------------------------------------------------------");
-                return false;
-            }
-                
-        }
-    }
-
-    CHI:printf("\nPlease Input Student's Chinese Grade:  "); scanf("%f",&t.ChineseScore); 
-    if(t.ChineseScore>100||t.ChineseScore<0)
-    {
-        ErrorGradePrint();
-        goto CHI;
-    }
-    MATH:printf("\nPlease Input Student's Math Grade:  "); scanf("%f",&t.MathScore); 
-    if(t.MathScore>100||t.MathScore<0)
-    {
-        ErrorGradePrint();
-        goto MATH;
-    }
-    ENG:printf("\nPlease Input Student's English Grade:  "); scanf("%f",&t.EnglishScore); 
-    if(t.EnglishScore>100||t.EnglishScore<0)
-    {
-        ErrorGradePrint();
-        goto ENG;
-    }
-    PHY:printf("\nPlease Input Student's Physics Grade:  "); scanf("%f",&t.PhysicsScore); 
-    if(t.PhysicsScore>100||t.PhysicsScore<0)
-    {
-        ErrorGradePrint();
-        goto PHY;
-    }
-    CHE:printf("\nPlease Input Student's Chemistry Grade:  "); scanf("%f",&t.ChemistryScore); 
-    if(t.ChemistryScore>100||t.ChemistryScore<0)
-    {
-        ErrorGradePrint();
-        goto CHE;
-    }
-    t.Score = t.ChemistryScore + t.ChineseScore + t.EnglishScore + t.MathScore + t.PhysicsScore ;
-    fseek(fp, 0, SEEK_END);
-    fprintf(fp, "%d;%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;\n",
-        t.StudentNum,
-        t.ChineseScore,
-        t.MathScore,
-        t.EnglishScore,
-        t.PhysicsScore,
-        t.ChemistryScore,
-        t.Score);
     return true;
 }
 
-int main()
+float calculateAverage(const Score& s) 
 {
-    InitiateFile();
-    FILE *fp;
-    if((fp = fopen("StudentGrade.dat","a+")) == NULL)
+    return (s.ChineseScore+s.MathScore+s.EnglishScore+s.PhysicsScore+s.ChemistryScore)/5.0f;
+}
+
+bool saveStudentToFile(const Score& s) 
+{
+    FILE* fp=fopen("StudentGrade.dat","ab");
+    if(!fp) 
     {
-        printf("--------------------------------------------------------------");
-        printf("\n\n\tThe file is not initialized. Please initialize it first.\n");
-        printf("\n--------------------------------------------------------------");
+        printf("\n\tFile opening failed!\n");
+        return false;
     }
-    while(InputStudenInformation(fp));
-    printf("\n");
-    system("pause");
+    bool success=(fwrite(&s,sizeof(Score),1,fp)==1);
+    fclose(fp);
+    return success;
+}
+
+int loadStudents(Score students[],int maxSize) 
+{
+    FILE* fp=fopen("StudentGrade.dat","rb");
+    if(!fp) return 0;
+
+    int count=0;
+    while(count<maxSize&&fread(&students[count],sizeof(Score),1,fp)==1) 
+    {
+        students[count].AveScore=calculateAverage(students[count]);
+        count++;
+    }
+    fclose(fp);
+    return count;
+}
+
+void quick_sort_total(Score q[],int l,int r) 
+{
+    if(l>=r) return;
+    int i=l-1,j=r+1;
+    float pivot=q[(l+r)/2].TotalScore;
+    while(i<j) 
+    {
+        do i++; while(q[i].TotalScore<pivot);
+        do j--; while(q[j].TotalScore>pivot);
+        if(i<j) swap(q[i],q[j]);
+    }
+    quick_sort_total(q,l,j);
+    quick_sort_total(q,j+1,r);
+}
+
+void quick_sort_average(Score q[],int l,int r) 
+{
+    if(l>=r) return;
+    int i=l-1,j=r+1;
+    float pivot=q[(l+r)/2].AveScore;
+    while(i<j) 
+    {
+        do i++; while(q[i].AveScore>pivot);
+        do j--; while(q[j].AveScore<pivot);
+        if(i<j) swap(q[i],q[j]);
+    }
+    quick_sort_average(q,l,j);
+    quick_sort_average(q,j+1,r);
+}
+
+void sortByTotalScore() 
+{
+    Score students[100];
+    int count=loadStudents(students,100);
+    if(count==0) 
+    {
+        printf("\n\tNo student data to sort!\n");
+        return;
+    }
+
+    quick_sort_total(students,0,count-1);
+    FILE* fp=fopen("StudentGrade.dat","wb");
+    if(!fp) 
+    {
+        printf("\n\tFile opening failed, sorting failed!\n");
+        return;
+    }
+    for(int i=0;i<count;i++) fwrite(&students[i],sizeof(Score),1,fp);
+    fclose(fp);
+    printf("\n\tSorting by total score completed!\n");
+}
+
+void sortByAverageScore() 
+{
+    Score students[100];
+    int count=loadStudents(students,100);
+    if(count==0) 
+    {
+        printf("\n\tNo student data to sort!\n");
+        return;
+    }
+
+    quick_sort_average(students,0,count-1);
+    FILE* fp=fopen("StudentGrade.dat","wb");
+    if(!fp) 
+    {
+        printf("\n\tFile opening failed, sorting failed!\n");
+        return;
+    }
+    for(int i=0;i<count;i++) fwrite(&students[i],sizeof(Score),1,fp);
+    fclose(fp);
+    printf("\n\tSorting by average score completed!\n");
+}
+
+bool getChoice(const char* prompt) 
+{
+    char choice;
+    printf("%s",prompt);
+    while(true) 
+    {
+        cin>>choice;
+        if(choice=='y'||choice=='Y') 
+        {
+            clearInputBuffer();
+            return true;
+        } else if(choice=='n'||choice=='N')
+        {
+            clearInputBuffer();
+            return false;
+        } else printf("\tInvalid input, please re-enter (y/n):  ");
+    }
+}
+
+bool InputStudentInformation() 
+{
+    Score s;
     system("cls");
-    printf("-----------------------------------------------------------------");
-    printf("\n\n\tDo you want to sort the data by grade values? : (y/n)");
-    char command;
-    do
-    {
-        command = getchar();
-    }while(command != 'Y' && command != 'y' && command != 'N' && command !='n');
-    if(command == 'Y' || command == 'y' ) 
-    {
-        StudentSort();
-        printf("\n-----------------------------------------------------------------\n");
-        printf("");
+    printf("\n\t===== Student Information Input =====");
+    printf("\n\n\tPlease enter student ID (enter 0 to end):  ");
+    if(scanf("%d",&s.StudentNum)!=1) {
+        printf("\n\tInput error! Please enter a numeric student ID!\n");
+        clearInputBuffer();
+        system("pause");
+        return true;
     }
-    else if(command == 'N' || command == 'n' )
+    if(s.StudentNum==0) return false;
+    Score temp[100];
+    int count=loadStudents(temp,100);
+    for(int i=0;i<count;i++) 
     {
+        if(temp[i].StudentNum==s.StudentNum) 
+        {
+            printf("\n\tStudent ID %d already exists!\n",s.StudentNum);
+            system("pause");
+            return true;
+        }
+    }
+    printf("\n\tPlease enter Chinese score (0-100):  ");
+    while(scanf("%f",&s.ChineseScore)!=1||s.ChineseScore<0||s.ChineseScore>100) 
+    {
+        printf("\tInvalid score, please re-enter (0-100):  ");
+        clearInputBuffer();
+    }
+    printf("\tPlease enter Math score (0-100):  ");
+    while(scanf("%f",&s.MathScore)!=1||s.MathScore<0||s.MathScore>100)
+    {
+        printf("\tInvalid score, please re-enter (0-100):  ");
+        clearInputBuffer();
+    }
+    printf("\tPlease enter English score (0-100):  ");
+    while(scanf("%f",&s.EnglishScore)!=1||s.EnglishScore<0||s.EnglishScore>100) 
+    {
+        printf("\tInvalid score, please re-enter (0-100):  ");
+        clearInputBuffer();
+    }
+
+    printf("\tPlease enter Physics score (0-100):  ");
+    while(scanf("%f",&s.PhysicsScore)!=1||s.PhysicsScore<0||s.PhysicsScore>100) {
+        printf("\tInvalid score, please re-enter (0-100):  ");
+        clearInputBuffer();
+    }
+
+    printf("\tPlease enter Chemistry score (0-100):  ");
+    while(scanf("%f",&s.ChemistryScore)!=1||s.ChemistryScore<0||s.ChemistryScore>100) {
+        printf("\tInvalid score, please re-enter (0-100):  ");
+        clearInputBuffer();
+    }
+
+    s.TotalScore=s.ChineseScore+s.MathScore+s.EnglishScore+s.PhysicsScore+s.ChemistryScore;
+    s.AveScore=calculateAverage(s);
+
+    if(saveStudentToFile(s)) printf("\n\tStudent information saved successfully!\n");
+    else printf("\n\tFailed to save student information!\n");
+    system("pause");
+    return true;
+}
+
+void printAllStudents() {
+    Score students[100];
+    int count=loadStudents(students,100);
+    if(count==0) {
+        printf("\n\tNo student data!\n");
+        return;
+    }
+
+    printf("\n======================= All Student Information ======================");
+    printf("\nID\tChinese\tMath\tEnglish\tPhysics\tChemistry\tTotal\tAverage\n");
+    for(int i=0;i<count;i++) {
+        printf("%d\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t\t%.1f\t%.1f\n",
+               students[i].StudentNum,
+               students[i].ChineseScore,
+               students[i].MathScore,
+               students[i].EnglishScore,
+               students[i].PhysicsScore,
+               students[i].ChemistryScore,
+               students[i].TotalScore,
+               students[i].AveScore);
+    }
+}
+
+int main() {
+    if(!initFile()) {
+        system("pause");
+        return 1;
+    }
+
+    printf("\n\tStart entering student information (enter 0 to end)\n");
+    while(InputStudentInformation());
+    system("cls");
+
+    if(getChoice("\tDo you want to sort by total score? (y/n):")) {
+        sortByTotalScore();
+        printAllStudents();
+        system("pause");
         system("cls");
-        printf("-----------------------------------------------------");
-        printf("\n\nThe program closes cause the user stops input.\n\n");
-        printf("-----------------------------------------------------");
     }
+
+    if(getChoice("\tDo you want to sort by average score? (y/n):")) {
+        sortByAverageScore();
+        printAllStudents();
+        system("pause");
+        system("cls");
+    }
+
+    printf("\n\tProgram ended!\n");
     return 0;
 }
