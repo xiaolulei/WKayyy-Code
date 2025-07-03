effd0979ad0cc1a162f6139041bda2a49941ecf3
“WKayyy” <w2570900770@outlook.com>
2025-07-03 11:24:56 +0800

    更新作业一

--- Code/大一期末小学期/实训1/test1.cpp
+++ Code/大一期末小学期/实训1/test1.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <algorithm>
 #include <cstdio>
+#include <random>
 using namespace std;
 
 struct Score 
@@ -17,10 +18,112 @@ struct Score
     float AveScore;
 };
 
+struct Student
+{
+    int StudentNum;
+    char StudentName[20];
+};
+
 void clearInputBuffer() 
 {
     int c;
     while((c=getchar())!='\n'&&c!=EOF);
+}
+
+void RadomStudentInfromationInput()//仅用于管理员输入数据进行debug
+{
+    FILE *fp;
+    fp = fopen("StudentInformation.dat","wb");
+    Student write;
+    do
+    {
+        printf("Please enter StudentNum:(Input 0 to exit)  ");
+        cin >> write.StudentNum;
+        if(write.StudentNum == 0)
+        {
+            exit(0);
+        }
+        printf("\nPlease enter StudentName:  ");
+        cin >> write.StudentName;
+        fwrite(&write,sizeof(Student),1,fp);
+    }while(1);
+}
+
+int StudentALL = 10;
+
+void RadomSelect()
+{
+    RadomStart:
+    FILE *fp , *temp_file;
+    temp_file = fopen("TempFile.dat","wb");
+    Student Read;//假定学生上限为100;
+    fp = fopen("StudentInformation.dat","r+b");
+    if(fp == NULL)
+    {
+        printf("================Radom Student==================");
+        printf("\nCan not open the StudentInformation.dat ! ...");
+        printf("\n=============================================\n");
+        exit(0);
+    }
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1,StudentALL); //假定有是个数据，如果有更更多可以把10改成更大的数字
+    int radom_int = dis(gen);
+    int StudentCounts = 0;
+    int iffind = 0;
+    rewind(fp);
+    while(fread(&Read,sizeof(Student),1,fp))
+    {
+        StudentCounts++;
+        if((StudentCounts == radom_int) && !iffind)
+        {
+            system("cls");
+            printf("=========Radom Student=========");
+            printf("\nStudent's Num:  %d",Read.StudentNum);
+            printf("\nStudent's Name: %s",Read.StudentName);
+            printf("\n===============================\n");
+            printf("press any key to continue ...");
+            clearInputBuffer();
+            getchar();
+            iffind = 1;
+            continue;
+        }
+        fwrite(&Read,sizeof(Student),1,temp_file);
+    }
+    fclose(fp);
+    fclose(temp_file);
+    if(iffind)
+    {
+        remove("StudentInformation.dat");
+        rename("TempFile.dat","StudentInformation.dat");
+        StudentALL--;
+    }
+    else
+    {
+        remove("TempFile.dat");
+        system("cls");
+        printf("=========Radom Student=========");
+        printf("\nCan not find this Student ! ...");
+        printf("\n===============================\n");
+        system("pause");
+        clearInputBuffer();
+    }
+    char RadomSortChoice;
+    do
+    {
+        system("cls");
+        printf("=========Radom Student=========");
+        printf("\nDo you want to select another one : (y/n)");
+        cin >> RadomSortChoice;
+    }while(RadomSortChoice != 'y' && RadomSortChoice != 'Y' && RadomSortChoice != 'n' && RadomSortChoice != 'N');
+    if(RadomSortChoice == 'y' || RadomSortChoice == 'Y')
+    {
+        goto RadomStart;
+    }
+    else
+    {
+        return ;
+    }
 }
 
 bool initFile() 
@@ -263,29 +366,53 @@ void printAllStudents() {
 }
 
 int main() {
-    if(!initFile()) {
-        system("pause");
-        return 1;
-    }
-
-    printf("\n\tStart entering student information (enter 0 to end)\n");
-    while(InputStudentInformation());
+    //RadomStudentInfromationInput();
     system("cls");
-
-    if(getChoice("\tDo you want to sort by total score? (y/n):")) {
-        sortByTotalScore();
-        printAllStudents();
-        system("pause");
-        system("cls");
+    printf("======Student Manage System======");
+    printf("\n1.Radom Select Student");
+    printf("\n2.Student Infromation Manage");
+    printf("\n=================================");
+    printf("\nPlease enter your choice:   ");
+    int mainChoice;
+    do
+    {
+    cin >> mainChoice;
+    }while(mainChoice!=1&&mainChoice!=2);
+    if(mainChoice == 1)
+    {
+        RadomSelect();
     }
+    else
+    {
+        if(!initFile()) 
+        {
+            system("pause");
+            return 1;
+        }
 
-    if(getChoice("\tDo you want to sort by average score? (y/n):")) {
-        sortByAverageScore();
-        printAllStudents();
-        system("pause");
+        printf("\n\tStart entering student information (enter 0 to end)\n");
+        while(InputStudentInformation());
         system("cls");
-    }
 
+        if(getChoice("\tDo you want to sort by total score? (y/n):")) 
+        {
+            sortByTotalScore();
+            printAllStudents();
+            system("pause");
+            system("cls");
+        }
+
+        if(getChoice("\tDo you want to sort by average score? (y/n):")) 
+        {
+            sortByAverageScore();
+            printAllStudents();
+            system("pause");
+            system("cls");
+        }
+    }
+    system("cls");
+    printf("======Student Manage System======");
     printf("\n\tProgram ended!\n");
+    printf("=================================");
     return 0;
 }
